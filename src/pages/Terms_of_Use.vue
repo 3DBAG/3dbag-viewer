e9b7b6c0c5938cd1387694678c4cfb50e2a6a3b0
Balázs Dukai <balazs.dukai@gmail.com>
2020-10-29 09:12:11 +0100

    Add terms of use

---
+++ src/pages/Terms_of_Use.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Terms of Use</h1>
+  <p>
+    What you currently see on this website and the data that you can donwload is meant
+    for internal use within the 3D geoinformation group only. Please look around,
+    check the data but don't share it just yet. Neither the files nor the link to the
+    website. We still need some time to iron things out.
+  </p>
+</template>
