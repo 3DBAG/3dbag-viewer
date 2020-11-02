079778c79e6daeae56ab7ac92867fbb5396fe5a1
jliempt <jordi_purdi@live.nl>
2020-11-02 20:41:19 +0100

    Use performance tests instead of benchmark.js library

--- src/benchmark/benchmark.js
+++ src/benchmark/benchmark.js
@@ -29,6 +29,17 @@ export class benchmarker {
 
 	async benchmark() {
 
+		const compressionTypes = [ "original", "draco", "gzip", "gzip-pre" ];
+		const tileIDs = [ '39862', '26118', '54290', '47253', '26474', '59880', '55103', '35044', '26441', '48910', '45791', '56056',
+			'40916', '31132', '25220', '76721', '44837', '27019', '22172', '21253', '23708', '21237', '31930', '19102', '66653',
+			'28580', '50812', '39952', '43555', '24914', '55797', '33197', '28907', '36303', '31731', '43306', '66527', '37743',
+			'61326', '23113', '39580', '31730', '45708', '54794', '56934', '44448', '35976', '28652', '74829', '23397', '19118',
+			'49104', '57846', '44028', '23386', '36812', '55243', '40235', '56048', '74830', '27721', '11407', '24943', '67596',
+			'55525', '59227', '23650', '32615', '21867', '33227', '51988', '45363', '32616', '27723', '51680', '67595', '24638',
+			'67290', '32617', '20041', '20650', '20038', '20651', '27722', '27416', '20039', '32922', '21561', '30997', '32923' ];
+
+		let average = ( array ) => array.reduce( ( a, b ) => a + b ) / array.length;
+
 		const b3dmloader = new B3DMLoader( );
 
 		b3dmloader.manager.addHandler( /\.gltf$/, {
@@ -48,83 +59,67 @@ export class benchmarker {
 
 		} );
 
-		const tileIDs = [ "32616", "28580", "39862" ];
-
-		const baseURI = "https://godzilla.bk.tudelft.nl/3dtiles/lod22_allattrs/tiles/";
-		const dracoURI = baseURI + "draco/tiles/";
-
 		var results = { "original": { "id": [], "mean": [], "size": [] },
-			"draco": { "id": [], "mean": [], "size": [], "comparison": { "absolute": [], "relative": [], "parse_absolute": [], "parse_relative": [] } } };
+			"draco": { "id": [], "mean": [], "size": [], "comparison": { "absolute": [], "relative": [], "parse_absolute": [], "parse_relative": [] } },
+			"gzip": { "id": [], "mean": [], "size": [], "comparison": { "absolute": [], "relative": [] } },
+			"gzip-pre": { "id": [], "mean": [], "size": [], "comparison": { "absolute": [], "relative": [] } },
+			"draco-gzip": { "id": [], "mean": [], "size": [], "comparison": { "absolute": [], "relative": [] } } };
 
-		const networkSpeed = 5; // MB/s
+		for ( var compressionType of compressionTypes ) {
 
-		for ( const tileID of tileIDs ) {
+			const baseURI = "http://0.0.0.0/" + compressionType + "/";
 
-			var originalTileURI = baseURI + tileID + ".b3dm";
-			var dracoTileURI = dracoURI + tileID + ".b3dm";
+			var tile_n = 0;
 
-			var originalBuf = await this.fetchTile( originalTileURI );
-			var originalMB = originalBuf.byteLength / 1000000;
-			var originalTile = await this.parseTile( b3dmloader, originalBuf );
+			for ( const tileID of tileIDs ) {
 
-			var dracoBuf = await this.fetchTile( dracoTileURI );
-			var dracoMB = dracoBuf.byteLength / 1000000;
-			var dracoTile = await this.parseTile( b3dmloader, dracoBuf );
+				var tileURI = baseURI + tileID + ".b3dm";
 
-			console.log( originalTile );
-			console.log( dracoTile );
+				console.log( "Initiate benchmark for " + tileID + ", " + compressionType );
 
-			var that = this;
+				var times = { "original": [], "draco": [], "gzip": [], "gzip-pre": [], "draco-gzip": [] };
 
-			console.log( "Initiate benchmark" );
+				var buf = await this.fetchTile( tileURI );
+				var tile = await this.parseTile( b3dmloader, buf );
 
-			const suite = new Benchmark.Suite( this );
-			suite
-				.add( "Parse original", function () {
+				for ( var i = 0; i < 20; i ++ ) {
 
-					that.parseTile( b3dmloader, originalBuf );
+					var d = new Date();
+					var start = d.getTime();
 
-				} )
-				.add( "Parse Draco", function () {
+					var buf = await this.fetchTile( tileURI );
+					var tile = await this.parseTile( b3dmloader, buf );
 
-					that.parseTile( b3dmloader, dracoBuf );
+					var d = new Date();
+					var end = d.getTime();
+					times.original.push( end - start );
 
-				} )
-				.on( "cycle", function ( event ) {
+				}
 
-					console.log( String( event.target ) );
+				var mb = buf.byteLength / 1000000;
+				var avg = average( times.original ) / 1000;
 
-				} )
-				.on( "complete", function () {
+				results[ compressionType ].id.push( tileID );
+				results[ compressionType ].mean.push( avg );
+				results[ compressionType ].size.push( mb );
 
-					results.original.id.push( tileID );
-					results.original.mean.push( this[ 0 ].stats.mean );
-					results.original.size.push( originalMB );
-					const originalTime = this[ 0 ].stats.mean + originalMB / networkSpeed;
+				if ( compressionType != "original" ) {
 
+					const originalTime = results.original.mean[ tile_n ];
+					results[ compressionType ].comparison.relative.push( avg / originalTime );
+					results[ compressionType ].comparison.absolute.push( avg - originalTime );
 
-					results.draco.id.push( tileID );
-					results.draco.mean.push( this[ 1 ].stats.mean );
-					results.draco.size.push( dracoMB );
-					const dracoTime = this[ 0 ].stats.mean + dracoMB / networkSpeed;
-					results.draco.comparison.relative.push( dracoTime / originalTime );
-					results.draco.comparison.absolute.push( dracoTime - originalTime );
-					results.draco.comparison.parse_absolute.push( this[ 1 ].stats.mean - this[ 0 ].stats.mean );
-					results.draco.comparison.parse_relative.push( this[ 1 ].stats.mean / this[ 0 ].stats.mean );
+				}
 
-				} )
-				.run( { async: false } );
+				tile_n += 1;
 
-		   }
+			}
 
-		   console.log( results );
+			console.log( results );
 
-		   for ( var i = 0; i < results.original.id.length; i ++ ) {
+		}
 
-			console.log( { "file size": results.original.size[ i ], "relative": results.draco.comparison.relative[ i ], "absolute": results.draco.comparison.absolute[ i ],
-			 "parse_relative:": results.draco.comparison.parse_relative, "parse_absolute": results.draco.comparison.parse_absolute } );
-
-		   }
+		console.log( results );
 
 	}
 
