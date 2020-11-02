5d735f2d28c026e197b92624a32249497fd90edf
Ravi Peters <r.y.peters@tudelft.nl>
2020-11-02 18:37:13 +0100

    lint

--- src/pages/Viewer.vue
+++ src/pages/Viewer.vue
@@ -37,7 +37,7 @@
     >
       <p class="is-size-7">
         <a
-					class="tag"
+          class="tag"
           href="https://docs.google.com/forms/d/e/1FAIpQLSe2XLCYNmoFVHrgt_uRXeLLwfzDK7gS2kE7mGH8rnk6ltE0LQ/viewform?"
           target="_blank"
           rel="noopener noreferrer"
@@ -45,7 +45,7 @@
           Give feedback
         </a> |
         <a
-					class="tag is-danger"
+          class="tag is-danger"
           :href="reportDataIssueUrl"
           target="_blank"
           rel="noopener noreferrer"
