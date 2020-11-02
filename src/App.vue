405cdfbb9f43a016503eefcb3b828d17978f48e0
Ravi Peters <r.y.peters@tudelft.nl>
2020-11-02 16:04:09 +0100

    add PDOK atribution and improve modal

--- src/App.vue
+++ src/App.vue
@@ -96,35 +96,45 @@
       <div class="modal-background" />
       <div class="modal-card">
         <header class="modal-card-head">
-          <p class="modal-card-title">Welcome to 3D BAG beta</p>
+          <p class="modal-card-title">
+            Welcome to 3D BAG v2 beta
+          </p>
           <button
             class="delete"
             aria-label="close"
-            @click="showWelcome"
+            @click="showWelcome = false"
           />
         </header>
         <section class="modal-card-body">
           <div class="content">
-            <h2>How to use</h2>
+            <p>This is the first beta release of the 3DBAG v2.</p>
             <ul>
-              <li>See the attributes of a building by double clicking on it!</li>
-              <li>This is a Beta version and we are improving on it every month. Come back often to see the updates! The version number is in the top left corner, besides the logo.</li>
+              <li>Everything is still under development. Expect broken things.</li>
+              <li>We'll release an improved version of 3DBAG at least once every month.</li>
+              <li>
+                Check the <router-link to="/docs#faq">
+                  FAQ
+                </router-link> for a list of known issues.
+              </li>
+              <li>If you find another issue you can use the feedback button to report it.</li>
+              <li>See the attributes of a building by clicking on it.</li>
             </ul>
 
-            <h2>Terms of use</h2>
+            <h2 class="title is-5">
+              Terms of use
+            </h2>
             <ul>
-              <li>The first rule about 3D BAG, is that you do not talk about 3D BAG.</li>
-              <li>The second rule about 3D BAG, is that you do not talk about 3D BAG.</li>
-              <li>The third rule about 3D BAG, is that you do not talk about 3D BAG.</li>
+              <li>Please do not publicly share or promote this website at this time.</li>
+              <li>Otherwise feel free to do with the data whatever you want.</li>
             </ul>
           </div>
         </section>
         <footer class="modal-card-foot">
           <button
-            class="button is-success"
+            class="button is-primary"
             @click="showWelcome = false"
           >
-            Gotcha!
+            Okay
           </button>
         </footer>
       </div>
